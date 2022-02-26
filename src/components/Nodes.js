0151e1d697d8b01491eaadb11fe92d36ba1febd5
ParkSangEon <sangeon1500@daum.net>
2022-02-26 17:03:13 +0900

    feat : Nodes componenet

---
+++ src/components/Nodes.js
@@ -0,0 +1,43 @@
+function Nodes({ $app, initialState, onClick }) {
+  this.state = initialState;
+  this.onClick = onClick;
+
+  this.$target = document.createElement('ul');
+  $app.appendChild(this.$target);
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+    this.render;
+  };
+
+  this.render = () => {
+    if (!this.state.nodes) return;
+    const nodesTemplate = this.state.nodes
+      .map(({ type, id, name }) => {
+        const iconPath = type === 'FILE ' ? './assets/file.png' : './assets/directory.png';
+        return `<div class="Node" data-node-id="${id}">
+        <img src="${iconPath}"/>
+        <div>${name}</div>
+      </div>`;
+      })
+      .join('');
+    this.$target.innerHTML = !this.state.isRoot
+      ? `<div class="Node">
+      <img src="/assets/prev.png"/>
+    </div>${nodesTemplate}`
+      : nodesTemplate;
+  };
+
+  [...this.$target.querySelectorAll('.Node')].forEach(($node) => {
+    $node.addEventListener('click', (e) => {
+      const { nodeId } = e.target.dataset;
+      const selectedNode = this.state.nodes.find(({ id }) => id === nodeId);
+
+      if (selectedNode) {
+        this.onClick(selectedNode);
+      }
+    });
+  });
+
+  this.render;
+}
